37305a6fcf9774c0d27ae3a0485091c5aba355eb
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-06-27 14:41:00 +0200

    2706

---
+++ 27.05 Loop/2/script.js
@@ -0,0 +1,36 @@
+// // 4. 
+
+// const d = [1, 2, 3, 4];
+// let a = +prompt('Number 1');
+// let c = 0;
+// for (let i = 0; i < d.length; i++) {
+//     if (a == d[i]) {
+//         break;
+//     }
+//     else {
+//         c += d[i];
+//     }
+// }
+// alert(c/(d.length));
+
+// 5. 
+// const d = [1, 2, 3, 4];
+// let a = 0;
+// for (let key in d) {
+//     if (a < d[key]) {
+//         a = d[key];
+//     }
+// }
+// alert(a);
+
+//6. 
+// const d = [1, 2, 3, 4];
+
+// for (let key of d) {
+//     alert(key);
+// }
+
+function getRandomArbitrary(min, max) {
+
+    return alert(Math.random() * (100 - 0) + 0);
+}
