dc68a8b8b65d857a7621b95e557ae98c2e789ff9
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-06-29 08:48:23 +0300

    2906

---
+++ 27.05 Loop/3/script.js
@@ -0,0 +1,75 @@
+// 7. Два автомобиля едут друг к другу с постоянными скоростями V1 и V2 км/ч.
+// Определите, через какое время автомобили встретятся, если расстояние между
+// ними равно S км. Добавить проверку на ввод только чисел
+
+// let a = +prompt('Number 1');
+// let b = +prompt('Number 2');
+// let c = +prompt('Number 3');
+// if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+//     let d = c / (a + b);
+//     alert(`${d} часов`);
+// }
+
+// else {
+//     alert('this not number');
+// }
+
+
+// 8. Пользователь вводит номер месяц. Необходимо вывести пору года по номеру
+// месяца (2 способа)
+
+// let a = +prompt('Number 1');
+// switch (true) {
+//     case a == 1 || a == 2 || a == 12:
+//         alert("Winter");
+//         break;
+//     case a > 2 || a <= 5:
+//         alert("Spring");
+//         break;
+//     case a > 5 || a <= 9:
+//         alert("Summer");
+//         break;
+//     case a > 9 || a <= 11:
+//         alert("Autumn");
+//         break;
+// }
+
+
+// let a = +prompt('Number 1');
+// switch (a) {
+//     case 1: case 2: case 12:
+//         alert("Winter");
+//         break;
+//     case 3: case 4: case 5:
+//         alert("Spring");
+//         break;
+//     case 6: case 7: case 8:
+//         alert("Summer");
+//         break;
+//     case 9: case 10: case 11:
+//         alert("Autumn");
+//         break;
+// }
+
+
+// let a = +prompt('Number 1');
+// if (a == 1 || a == 2 || a == 12) {
+//     alert("Winter");
+// }
+// else if (a > 2 && a <= 5) {
+//     alert("Spring");
+
+// }
+// else if (a > 5 && a <= 9) {
+//     alert("Summer");
+// }
+// else if (a > 9 && a <= 11) {
+//     alert("Autumn");
+// }
+
+// 9. Пользователь вводит число: 1, 2, 3 или 4. Если это значение '1',
+//  то вывести в консоль
+// // 'зима', '2' – 'весна' и так далее. Добавить проверку на ввод 
+// только чисел. Решите
+// // задачу через switch-case. 
+
