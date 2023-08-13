7ce1f6041177f50f41df5072a2db19f4a4bdd971
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-08-13 21:18:58 +0300

    1308

---
+++ 01.06 Functions/6/script.js
@@ -0,0 +1,7 @@
+// Вы вводите 2 числа a и b. Вам необходимо решить 
+// пример (2(а-b)-4(b-a))/2
+
+const a = +prompt('Number 1');
+const b = +prompt('Number 2');
+
+alert((2 * (a - b) - 4 * (b - a)) / 2);
