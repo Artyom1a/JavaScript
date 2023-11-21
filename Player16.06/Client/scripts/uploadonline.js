d20cbac6c9038b638fcb1c2f1f01bad7cbac2340
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-11-21 17:15:19 +0300

    2111

---
+++ Player16.06/Client/scripts/uploadonline.js
@@ -0,0 +1,14 @@
+document.querySelector('button').addEventListener('click', () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://localhost:7057/Account');
+    xhr.send();
+    xhr.onload = () => {
+        const arr = JSON.parse(xhr.response);
+        const weather = document.querySelector(".weather");
+        for (i = 0; i < arr.length; i++) {
+            let span = document.createElement('span');
+            span.textContent = JSON.stringify(arr[i]);
+            weather.appendChild(span);
+        }
+    };
+})
