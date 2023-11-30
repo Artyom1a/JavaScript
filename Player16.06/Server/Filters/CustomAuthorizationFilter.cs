c888cd4e5c50e099e02c483ba10ea2fcd7da2527
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-11-30 22:49:07 +0300

    3011

---
+++ Player16.06/Server/Filters/CustomAuthorizationFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Server.Filters
+{
+    public class CustomAuthorizationFilter : IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!IsUserAuthorized(context))
+            {
+                context.Result = new UnauthorizedResult();
+            }
+        }
+
+        private bool IsUserAuthorized(AuthorizationFilterContext context)
+        {
+            var token = context.HttpContext.Request.Headers["Authorization"];
+            return !string.IsNullOrEmpty(token);
+        }
+    }
+}
+
+
+
+
+
+
