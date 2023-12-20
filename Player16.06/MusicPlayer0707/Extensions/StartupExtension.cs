f0150bd050658324a5200b4e1c8b3ad64b4a78e5
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-12-20 17:20:55 +0300

    1912

---
+++ Player16.06/MusicPlayer0707/Extensions/StartupExtension.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using OnlineShop.Repositories;
+using OnlineShop.Services;
+using System.Configuration;
+
+namespace OnlineShop.Extensions
+{
+
+    public static class StartupExtension
+    {
+        public static void AddRepositories(this IServiceCollection services, string connectionString)
+        {
+            services.AddSingleton(_ => new MySqlConnection(connectionString));
+            services.AddTransient<UserRepository>();
+        }
+
+        public static void AddServices(this IServiceCollection services)
+        {
+
+            services.AddTransient<UserServices>();
+            services.AddTransient<AccountService>();
+        }
+
+
+    }
+}
