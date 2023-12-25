a61d385dfea6148e1c57a71cbc49fe84b9c75fee
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-12-25 11:35:13 +0300

    2512

---
+++ Player16.06/MusicPlayer0707/Models/Repository/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OnlineShop.Models.Repository
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
+
+
+
