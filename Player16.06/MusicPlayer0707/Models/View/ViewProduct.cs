687001db7a0403559c3180b58a61eb3be894a281
Artyom Shevchik <artyom.shevchik16@gmail.com>
2023-12-27 13:48:14 +0300

    2712

---
+++ Player16.06/MusicPlayer0707/Models/View/ViewProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShop.Models.View
+{
+    public class ViewProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string category { get; set; }
+        public string brand { get; set; }
+    }
+}
