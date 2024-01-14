1ac13fa9876daf9a9f19f0c4d8f358bcb32b89c2
Artyom Shevchik <artyom.shevchik16@gmail.com>
2024-01-14 11:41:52 +0300

    1401

---
+++ Player16.06/OnlineShop/Repositories/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace OnlineShop.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly MySqlConnection m_Connection;
+
+        public BaseRepository(MySqlConnection connection)
+        {
+            m_Connection = connection;
+        }
+    }
+}
