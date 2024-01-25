78ce67ffc4b705e35001ce6a36993f15c472c00a
Artyom Shevchik <artyom.shevchik16@gmail.com>
2024-01-25 13:32:22 +0300

    2501

---
+++ Player16.06/OnlineShop/Models/Repository/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineShop.Models.Repository
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        public int category_id { get; set; }
+
+        public int brand_id { get; set; }
+    }
+}
+
---
+++ Player16.06/OnlineShop/Models/View/ViewOrder.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShop.Models.View
+{
+    public class ViewOrder
+    {
+        public int Id { get; set; }
+        public int User_Id { get; set; }
+        public int Product_Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Count { get; set; }
+        public int Discount { get; set; }
+    }
+}
