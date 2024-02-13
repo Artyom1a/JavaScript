a51d216fbb0cc44dbf6880d39e535e718f5e8ee6
Artyom Shevchik <artyom.shevchik16@gmail.com>
2024-02-13 15:35:38 +0300

    1302

---
+++ Player16.06/WebApplication1/Client/scripts/profile.js
@@ -0,0 +1,13 @@
+document.querySelector('button').addEventListener('click', async () => {
+    const response = await fetch(`https://localhost:7156/WeatherForecast`, {
+        method: 'GET',
+        headers: { 'Authorization': '1234' }
+    })
+    const json = await response.json();
+    const weather = document.querySelector(".weather");
+    for (let item of json) {
+        let div = document.createElement("div");
+        div.textContent = JSON.stringify(item);
+        weather.appendChild(div);
+    }
+});
