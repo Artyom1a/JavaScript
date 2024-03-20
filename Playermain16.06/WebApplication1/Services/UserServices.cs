7421ceed319e804f29bb234324642196a5919e2a
Artyom Shevchik <artyom.shevchik16@gmail.com>
2024-03-20 11:00:17 +0300

    feature2003n1

---
+++ Playermain16.06/WebApplication1/Services/UserServices.cs
@@ -0,0 +1,31 @@
+﻿using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace WebApplication1.Services
+{
+    public class UserServices
+    {
+        private readonly UserRepository m_userrep;
+        public UserServices(UserRepository repository)
+        {
+            m_userrep = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public List<User> GetAll()
+        {
+            return m_userrep.GetAll();
+        }
+
+        public void Create(User user)
+        {
+            var list = m_userrep.GetAll();
+            var item = list.FirstOrDefault(x => x.Email == user.Email);
+            if (item != null)
+            {
+                throw new Exception();
+            }
+            //list.Add(user);
+            m_userrep.WriteNew(user);
+        }
+    }
+}
